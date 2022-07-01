7cbb00e8b0e35512fcd48fe9aac6d31b0c6740e9
manyamvamsi <103053909+manyamvamsi@users.noreply.github.com>
2022-07-01 19:59:09 +0530

    update

---
+++ input.tf
@@ -0,0 +1,24 @@
+variable "subscription_id" {
+    type = string
+    default = "e39d9f61-ec63-4a99-ba8e-43e6f83a9603"
+    description = "IAC subscription id"
+
+}
+variable "client_id" {
+    type = string
+    default = "3c26e904-3a50-4a5d-9d10-61f1613b5485"
+    description = "IAC client id"
+
+}
+variable "client_secret" {
+    type = string
+    default = "xcg8Q~x~LhnasWblWZ~aa2GkT-OjtBgPbM7rmaqn"
+    description = "IAC client secret"
+  
+}
+variable "tenant_id" {
+    type = string
+    default = "a3dcc283-335d-4394-b69a-b7b73e45b6b3"
+    description = "IAC tenant id"
+  
+}
